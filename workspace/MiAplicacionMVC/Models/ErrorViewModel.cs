6ae87c6c419c8cd1f44d1c6bf1187f3e8577f258
Leandro C. Tomassini <leandroctomassini@gmail.com>
2024-01-28 18:03:46 -0300

    Portfolio

---
+++ workspace/MiAplicacionMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiAplicacionMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ workspace/MiAplicacionMVC/Models/Persona.cs
@@ -0,0 +1,8 @@
+namespace MiAplicacionMVC.Models;
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
